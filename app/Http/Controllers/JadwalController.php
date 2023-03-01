5c265d629f4246ad6d98922c8369df7beb9d4349
jenalalfianta <jenalalfianta@gmail.com>
2023-03-01 07:36:43 +0700

    crud jadwal done

--- app/Http/Controllers/JadwalController.php
+++ app/Http/Controllers/JadwalController.php
@@ -99,6 +99,6 @@ class JadwalController extends Controller
     public function destroy(Jadwal $jadwal)
     {
         $event = Jadwal::find($jadwal->id)->delete();
-        return redirect('/jadwal#calendar')->with('message','Jadwal Berhasil Ditambahkan :)');
+        return redirect('/jadwal#calendar')->with('message','Jadwal Berhasil Dihapus :)');
     }
 }
